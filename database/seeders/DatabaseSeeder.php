1ea52d01b9ff4483b3a01645931ccd05aa3ab11a
Pawal Karki <pawal_karki@icloud.com>
2025-09-03 22:03:42 +0545

    update: database:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@taraharautsav.com',
+            'password' => Hash::make('P@ssw0rd123'),
+            'role' => 'admin',
         ]);
 
         $this->call([
